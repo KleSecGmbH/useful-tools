daab8d10a4a98ef426494651f3f20fbd07bfa13a
KleSecGmbH <91429214+KleSecGmbH@users.noreply.github.com>
2023-10-01 13:55:12 +0200

    Create docker-remove-all.sh

---
+++ docker-remove-all.sh
@@ -0,0 +1,14 @@
+# Alle Container stoppen
+docker stop `docker ps -qa`
+
+# Alle Container entfernen
+docker rm `docker ps -qa`
+
+# Alle Images entfernen
+docker rmi -f `docker images -qa `
+
+# Alle Volumes entfernen
+docker volume rm $(docker volume ls -qf dangling="true")
+
+# Alle Netzwerke entfernen
+docker network rm `docker network ls -q`
